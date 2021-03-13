4e0577c278e47b08ac8c038877f34120b388aa6f
nicksullivan21 <74960495+nicksullivan21@users.noreply.github.com>
2021-03-13 13:28:45 +0800

    Added schema.sql

---
+++ Schema/Schema.sql
@@ -0,0 +1,22 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "final_net_migration" (
+    "Migration_Type" VARCHAR   NOT NULL,
+    "Region_X" VARCHAR   NOT NULL,
+    "Year" INT   NOT NULL,
+    "Sex" VARCHAR   NOT NULL,
+    "Age" VARCHAR   NOT NULL,
+    "Value" INT   NOT NULL
+);
+
+CREATE TABLE "final_enrolment_data" (
+    "Region" VARCHAR   NOT NULL,
+    "Year" INT   NOT NULL,
+    "Num_Enrolled" INT   NOT NULL
+);
+
+ALTER TABLE "final_net_migration" ADD CONSTRAINT "fk_final_net_migration_Region_X_Year" FOREIGN KEY("Region_X", "Year")
+REFERENCES "final_enrolment_data" ("Region", "Year");
+
